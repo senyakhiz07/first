944dfd76f372976051026f56f98ba451e6109ed3
Ksenya <hksyusha07@gmail.com>
2026-09-10 09:03:11 +0400

    исправлен интерфейс пользователя

--- first.cpp
+++ first.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+int Square(int a, int h) {
+    return 0.5 * a * h;
+}
+
 int main() {
     std::cout << "Hello world!" << std::endl;
     return 0;
+    std::cout << "¬ведите сторону и высоту треугольника";
+    std::cin >> a;
+    std::cin >> h;
+    std::cout << "ѕлощадь треугольника" << Square(a, h);
 }
